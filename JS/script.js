f1191606a117e58dda2eec84ff12d7048e06ae04
Jrm Treiber <72030730+jrmtreiber@users.noreply.github.com>
2025-03-01 15:59:07 -0300

    UP.

---
+++ JS/script.js
@@ -0,0 +1,56 @@
+let currentIndex = 0;
+let intervalId;
+
+function showSlide(index) {
+    const slides = document.querySelectorAll('.carousel-item');
+    if (index >= slides.length) {
+        currentIndex = 0;
+    } else if (index < 0) {
+        currentIndex = slides.length - 1;
+    } else {
+        currentIndex = index;
+    }
+    const newTransform = `translateX(-${currentIndex * 100}%)`;
+    document.querySelector('.carousel-images').style.transform = newTransform;
+}
+
+function moveSlide(direction) {
+    clearInterval(intervalId);
+    showSlide(currentIndex + direction);
+    startAutoSlide();
+}
+
+function startAutoSlide() {
+    intervalId = setInterval(() => {
+        showSlide(currentIndex + 1);
+    }, 10000);
+}
+
+setInterval(() => {
+    showSlide(currentIndex + 1);
+}, 10000);
+
+showSlide(currentIndex);
+
+// 
+
+window.onscroll = function () {
+    stickyHeader();
+};
+
+var header2 = document.getElementById("header2");
+
+function stickyHeader() {
+    if (window.innerWidth > 768) {
+        if (window.pageYOffset > 100) {
+            header2.style.position = "fixed";
+            header2.style.top = "0";
+        } else {
+            header2.style.position = "absolute";
+            header2.style.top = "auto";
+        }
+    } else {
+        header2.style.position = "absolute";
+        header2.style.top = "auto";
+    }
+}
