15cfc2fdf6d97f6e1bc81748e316658cdd96cea1
Jrm Treiber <72030730+jrmtreiber@users.noreply.github.com>
2025-03-02 00:40:36 -0300

    ATT 6.

--- JS/script.js
+++ JS/script.js
@@ -54,3 +54,71 @@ function stickyHeader() {
         header2.style.top = "auto";
     }
 }
+
+// 
+
+// Produtos
+const produtos = [
+    { titulo: "Teclado", descricao: "Descrição", imagem: "./IMGS/banner4.png" },
+    { titulo: "Mouse", descricao: "Descrição", imagem: "./IMGS/banner4.png" },
+    { titulo: "Fone", descricao: "Descrição", imagem: "./IMGS/banner4.png" },
+];
+
+const produtosPorPagina = 3;  
+let paginaAtual = 1; 
+let produtosFiltrados = produtos;  
+
+function mostrarProdutos() {
+    const containerProdutos = document.getElementById("produtos");
+    containerProdutos.innerHTML = '';  
+
+    const inicio = (paginaAtual - 1) * produtosPorPagina;
+    const fim = inicio + produtosPorPagina;
+    const produtosPagina = produtosFiltrados.slice(inicio, fim);
+
+    if (produtosPagina.length === 0) {
+        document.getElementById("notFoundMessage").style.display = 'block';
+    } else {
+        document.getElementById("notFoundMessage").style.display = 'none';
+    }
+
+    produtosPagina.forEach(produto => {
+        const produtoDiv = document.createElement("div");
+        produtoDiv.classList.add("produto");
+
+        produtoDiv.innerHTML = `
+            <div class="imagemProduto">
+                <img src="${produto.imagem}" alt="">
+            </div>
+            <div class="descricaoProdutos">
+                <h1>${produto.titulo}</h1>
+                <p>${produto.descricao}</p>
+            </div>
+            <div class="botao">
+                <a href=""><button>COMPRAR</button></a>
+            </div>
+        `;
+
+        containerProdutos.appendChild(produtoDiv);
+    });
+
+    const totalPaginas = Math.ceil(produtosFiltrados.length / produtosPorPagina);
+
+    document.getElementById("paginaAtual").textContent = `Página ${paginaAtual}/${totalPaginas}`;
+}
+
+function alterarPagina(quantidade) {
+    const totalPaginas = Math.ceil(produtosFiltrados.length / produtosPorPagina);
+
+    paginaAtual += quantidade;
+
+    if (paginaAtual < 1) {
+        paginaAtual = 1;
+    } else if (paginaAtual > totalPaginas) {
+        paginaAtual = totalPaginas;
+    }
+
+    mostrarProdutos();
+}
+
+mostrarProdutos();
